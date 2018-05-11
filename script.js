3088bc14bc84c17aee02db069b14ca5f5b68dc43
Johnson <johnnyjp2070@gmail.com>
2018-05-11 12:22:13 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit = (temperatureInCelsius * 9)/ 5 + 32;
+console.log("Temperature of  "+temperatureInCelsius+ " celsius, will be "+celsiusToFahrenheit+ " fahrenheit");
+var temperatureInFahrenheit = 104;
+var fahrenheitToCelsius = (temperatureInFahrenheit - 32)* 5 / 9;
+console.log("Temperature of "+temperatureInFahrenheit+"  Fahrenheit, will be  "+fahrenheitToCelsius+"  Celsius");
+
